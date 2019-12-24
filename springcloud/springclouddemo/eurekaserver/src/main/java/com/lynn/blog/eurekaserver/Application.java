c25e2f40ee6ed6f5e5019029f684f73de9394e6e
lvhongbing <781072481@qq.com>
2019-12-24 11:22:01 +0800

    Create Application.java

---
+++ springcloud/springclouddemo/eurekaserver/src/main/java/com/lynn/blog/eurekaserver/Application.java
@@ -0,0 +1,19 @@
+package com.lynn.blog.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+@EnableEurekaServer
+@SpringCloudApplication
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
